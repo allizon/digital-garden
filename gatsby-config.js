bcd00f32d37a686687c9313266aae353d6e1e96b
Allison Holt <hey@alli.fyi>
2022-03-04 17:17:56 -0500

    Getting tailwind to work

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Digital Garden`,
+    description: ``,
+    author: `Axxxxxx Holt`,
+    siteUrl: `https://allizon.github.io/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("./tailwind.config.js"),
+        ],
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
