b69db049a2ec194a109f08fb8499ca3d830846ad
Allison Holt <hey@alli.fyi>
2022-03-05 11:40:51 -0500

    Hooking up web fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
-const colors = require("tailwindcss/colors")
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["./src/pages/**/*.js", "./src/components/**/*.js"],
   theme: {
     fontFamily: {
-      sans: ["Avenir Next"],
+      sans: ["Roboto", "Avenir Next"],
+      body: ["Roboto"],
+      display: ["Roboto Slab"],
       serif: ["Georgia"],
     },
   },
-}
+};
