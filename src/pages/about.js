13e7893062de0640bc8e66a760d946750f91cd2f
Allison Holt <100834+allizon@users.noreply.github.com>
2022-03-05 22:12:40 -0500

    Adding "about" page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+const About = () => (
+  <Layout>
+    <h1>About</h1>
+  </Layout>
+);
+
+export default About;
+
