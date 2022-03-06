fb66c2f43695e621db29aa85e17c1ebb63f83fa4
Allison Holt <hey@alli.fyi>
2022-03-05 21:23:53 -0500

    Making cascade layers work, then backing them out for now

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,11 +2,22 @@ import React from "react";
 import { Link } from "gatsby";
 
 const Footer = () => (
-  <footer className="mt-8 bg-gray-800 text-white">
-    <div className="max-w-4xl m-0 mx-auto py-8 px-5">
-      © {new Date().getFullYear()}, Built with
-      {` `}
-      <a href="https://www.gatsbyjs.com">Gatsby</a>
+  <footer className="mt-8 bg-gray-800 text-white text-sm">
+    <div className="max-w-4xl m-0 mx-auto py-8 px-5 flex">
+      <div className="w-1/2">
+        © {new Date().getFullYear()} Allison Holt.
+        <br />
+        Built with <a href="https://www.gatsbyjs.com">Gatsby</a>.
+      </div>
+      <div className="w-1/2 text-right">
+        <a href="https://twitter.com/AllizonPrime">twitter</a>
+        <br />
+        <a href="https://instagram.com/allizon_prime">instagram (personal)</a>
+        <br />
+        <a href="https://instagram.com/allizon_arts">instagram (art)</a>
+        <br />
+        <a href="https://github.com/allizon">github</a>
+      </div>
     </div>
   </footer>
 );
